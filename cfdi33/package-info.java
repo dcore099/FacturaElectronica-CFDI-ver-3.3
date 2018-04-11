448802d22df556038fd41836224f9b17eb295f97
dcore099 <dcore099@gmail.com>
2018-04-11 08:40:28 -0500

    Mejoras en la librería
    Agregué cambios en package-info para los nameSpace y agregué soporte para el complemento de recepción de pagos.

---
+++ cfdi33/package-info.java
@@ -0,0 +1,24 @@
+//
+// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.8-b130911.1802 
+// Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: 2017.06.22 a las 09:26:52 AM CDT 
+//
+
+//@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.sat.gob.mx/cfd/33", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+@XmlSchema( 
+	    //namespace = "http://www.sat.gob.mx/cfd/3", 
+	    elementFormDefault = XmlNsForm.QUALIFIED,
+	    		xmlns={
+	    	            @XmlNs(namespaceURI = "http://www.sat.gob.mx/cfd/3", prefix = "cfdi"),
+	    	            @XmlNs(namespaceURI = "http://www.sat.gob.mx/TimbreFiscalDigital", prefix = "tfd"),
+	    	            @XmlNs(namespaceURI = "http://www.sat.gob.mx/Pagos", prefix = "pago10"),
+	    	            //@XmlNs(namespaceURI = "http://www.buzonfiscal.com/ns/addenda/bf/2", prefix = "bfa2"),
+	    	            @XmlNs(namespaceURI = "http://www.w3.org/2001/XMLSchema-instance", prefix = "xsi")
+	    	    }
+		) 
+package mx.gob.seseqro.cfdi33;
+
+import javax.xml.bind.annotation.XmlNs;
+import javax.xml.bind.annotation.XmlNsForm;
+import javax.xml.bind.annotation.XmlSchema;
